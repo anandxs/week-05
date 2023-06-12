5707d7070839835e47376775acb5627ad9d43058
Anand S <anandsatheeshk@gmail.com>
2023-06-12 16:21:43 +0530

    added count property and contain function

--- StackProgram/Program.cs
+++ StackProgram/Program.cs
@@ -5,14 +5,17 @@
 		private int top;
 		private T[] arr;
 		private int capacity;
+		private int count;
 
 		public int Top { get { return top; } }
+		public int Count { get { return count; } }
 
         public Stack()
         {
 			capacity = 4;
 			top = -1;
 			arr = new T[capacity];
+			count = 0;
         }
 
 		public void Push(T data)
@@ -27,6 +30,7 @@
 
 			top++;
 			arr[top] = data;
+			count++;
 		}
 
 		public T Peek()
@@ -44,8 +48,20 @@
 
 			T poppedValue = arr[top];
 			top--;
+			count--;
 
 			return poppedValue;
+		}
+
+		public bool Contain(T value)
+		{
+			for (int i = top; i > -1; i--)
+			{
+				if (arr[i].Equals(value))
+					return true;
+			}
+
+			return false;
 		}
 
 		public void Print()
@@ -62,6 +78,11 @@
 	{
 		static void Main(string[] args)
 		{
-		}
+			Stack<int> s = new Stack<int>();
+            Console.WriteLine(s.Contain(1));
+			s.Push(1);
+			s.Push(10);
+			Console.WriteLine(s.Contain(2));
+        }
 	}
 }
