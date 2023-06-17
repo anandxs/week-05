18396f12d57d24a1553eec10c91979a24dc7a471
Anand S <anandsatheeshk@gmail.com>
2023-06-17 16:26:28 +0530

    added selection sort

---
+++ SelectionSortProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SelectionSortProgram
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 5, 4, 3, 2, 1 };
+
+			SelectionSort(arr);
+
+			foreach (int num in arr)
+			{
+                Console.Write(num + " ");
+            }
+		}
+
+		private static void SelectionSort(int[] arr)
+		{
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				for (int j = i + 1; j < arr.Length; j++)
+				{
+					if (arr[i] > arr[j])
+					{
+						Swap(arr, i, j);
+					}
+				}
+			}
+		}
+
+		private static void Swap(int[] arr, int i, int j)
+		{
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+}
